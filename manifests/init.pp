8d7586d342a9cc4ab2a35e5027502596d3dc98a0
eethak2 <eethak2@gmail.com>
2017-04-11 00:28:46 +0300

    committi

---
+++ manifests/init.pp
@@ -0,0 +1,19 @@
+class puppetti {
+        package { 'openssh-server':
+                ensure => "installed",
+        }
+
+         file { '/etc/ssh/sshd_config':
+                content => template("puppetti/sshd_config"),
+                require => Package["openssh-server"],
+                notify => Service["ssh"],
+        }
+
+        service { 'ssh':
+                ensure => 'running',
+                enable => 'true',
+                require => Package["openssh-server"],
+
+                
+        }
+}
